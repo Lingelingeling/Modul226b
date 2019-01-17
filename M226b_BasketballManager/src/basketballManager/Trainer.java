d572834964e6222320d381f1d48cec13a90303fa
Lingelingeling <33859776+Lingelingeling@users.noreply.github.com>
2019-01-17 11:21:27 +0100

    Update Trainer.java
    comments added

--- M226b_BasketballManager/src/basketballManager/Trainer.java
+++ M226b_BasketballManager/src/basketballManager/Trainer.java
@@ -22,29 +22,49 @@ public abstract class Trainer extends Person implements Trainable{
         this.trainExperience = trainExperience;
     }
 
+	/**
+	 * Function to increase the Defense of the chosen Player (Used in Main)
+	 */
     public void trainDefense(Player player){
         int stat = player.getDefending();
         stat += 2;
         player.setDefending(stat);
     }
+    
+    /**
+     * Function to increase the Passing of the chosen Player (Used in Main)
+     */
     public void trainPassing(Player player){
         int stat = player.getPassing();
         stat += 2;
         player.setPassing(stat);
     }
+    
+    /**
+     * Function to increase the Shooting of the chosen Player (Used in Main)
+     */
     public void trainShooting(Player player){
         int stat = player.getShooting();
         stat += 2;
         player.setShooting(stat);
     }
+    
+    /**
+     * Function to increase the Pace of the chosen Player (Used in Main)
+     */
     public void trainPace(Player player){
         int stat = player.getPace();
         stat += 2;
         player.setPace(stat);
     }
+    
+    /**
+     * Function to increase the Dribbling of the chosen Player (Used in Main)
+     */
     public void trainDribbling(Player player){
         int stat = player.getDribbling();
         stat += 2;
         player.setDribbling(stat);
     }
 }
+
